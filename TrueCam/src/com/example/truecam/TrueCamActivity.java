8f112c467a9a59100565580b78261533d20dac77
zhang xu <xu.zhang@orange.com>
2014-05-29 15:52:04 +0800

    create activity to take pics

--- TrueCam/src/com/example/truecam/TrueCamActivity.java
+++ TrueCam/src/com/example/truecam/TrueCamActivity.java
@@ -1,22 +1,116 @@
 package com.example.truecam;
 
-import android.os.Bundle;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class TrueCamActivity extends Activity {
 
+	private View mTrueCamView;
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	public static final int MEDIA_TYPE_VIDEO = 2;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Log.d("MyCameraApp", "Init truecam.... ");
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_true_cam);
+		mTrueCamView = LayoutInflater.from(this).inflate(
+				R.layout.true_cam_layout, null);
+		Button mTakePicBtn = (Button) mTrueCamView
+				.findViewById(R.id.btn_take_pic);
+		mTakePicBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				switch (v.getId()) {
+				case R.id.btn_take_pic: {
+					Log.d("MyCameraApp", "clicked button to launch camera");
+					Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+					Uri fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+					intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+					startActivityForResult(intent,
+							CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+					break;
+				}
+				}
+			}
+
+		});
+		setContentView(mTrueCamView);
+	}
+
+
+
+
+	/** Create a file Uri for saving an image or video */
+	private static Uri getOutputMediaFileUri(int type) {
+		return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(int type) {
+		// To be safe, you should check that the SDCard is mounted
+		// using Environment.getExternalStorageState() before doing this.
+
+		File mediaStorageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				"MyCameraApp");
+		// This location works best if you want the created images to be shared
+		// between applications and persist after your app has been uninstalled.
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d("MyCameraApp", "failed to create directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		File mediaFile;
+		if (type == MEDIA_TYPE_IMAGE) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "IMG_" + timeStamp + ".jpg");
+		} else if (type == MEDIA_TYPE_VIDEO) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "VID_" + timeStamp + ".mp4");
+		} else {
+			return null;
+		}
+
+		return mediaFile;
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.true_cam, menu);
-		return true;
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+			Log.d("MyCameraApp", "cam activity resulted");
+			if (resultCode == RESULT_OK) {
+				// Image captured and saved to fileUri specified in the Intent
+				Toast.makeText(this,
+ "Image saved to local directory",
+						Toast.LENGTH_LONG).show();
+			} else if (resultCode == RESULT_CANCELED) {
+				// User cancelled the image capture
+			} else {
+				// Image capture failed, advise user
+			}
+		}
 	}
 
 }
